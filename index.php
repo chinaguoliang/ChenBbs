a49e59afaaad0f05114e41b7d31adf5afc93bbe9
chen <38033919@qq.com>
2016-07-17 20:50:43 +0800

    修改入口文件

--- index.php
+++ index.php
@@ -20,9 +20,6 @@ define('APP_DEBUG',True);
 // 定义应用目录
 define('APP_PATH','./Application/');
 
-// 定义后台管理目录
-define('ADMIN_PATH','./Admin/');
-
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 
